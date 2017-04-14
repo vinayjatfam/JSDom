fce009cd5a3c7bad6f27e34e10c898f630d1d60c
vinayjatfam <rj.vinayy@gmail.com>
2017-04-14 13:13:49 +0530

    First Commit

---
+++ task/dom.js
@@ -0,0 +1,327 @@
+
+(function(){
+  
+  "use strict";
+
+        var flightDetails =  {
+			"return_on": [{
+				"flight_no": "SG-113",
+				"flight_name": "SpiceJet",
+				"depart_time": 1314924180000,
+				"arrival_time": 1314930780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 3953
+			}, {
+				"flight_no": "6E-118",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314945780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 1753
+			}, {
+				"flight_no": "IT-131",
+				"flight_name": "Kingfisher",
+				"depart_time": 1314938580000,
+				"arrival_time": 1314942180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 2819
+			}, {
+				"flight_no": "6E-124",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314960180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 9907
+			}, {
+				"flight_no": "SG-113",
+				"flight_name": "SpiceJet",
+				"depart_time": 1314924180000,
+				"arrival_time": 1314930780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 3953
+			}, {
+				"flight_no": "6E-118",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314945780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 1753
+			}, {
+				"flight_no": "IT-131",
+				"flight_name": "Kingfisher",
+				"depart_time": 1314938580000,
+				"arrival_time": 1314942180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 2819
+			}, {
+				"flight_no": "6E-124",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314960180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 9907
+			}, {
+				"flight_no": "SG-113",
+				"flight_name": "SpiceJet",
+				"depart_time": 1314924180000,
+				"arrival_time": 1314930780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 3953
+			}, {
+				"flight_no": "6E-118",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314945780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 1753
+			}, {
+				"flight_no": "IT-131",
+				"flight_name": "Kingfisher",
+				"depart_time": 1314938580000,
+				"arrival_time": 1314942180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 2819
+			}, {
+				"flight_no": "6E-124",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314960180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 9907
+			}, {
+				"flight_no": "SG-113",
+				"flight_name": "SpiceJet",
+				"depart_time": 1314924180000,
+				"arrival_time": 1314930780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 3953
+			}, {
+				"flight_no": "6E-118",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314945780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 1753
+			}, {
+				"flight_no": "IT-131",
+				"flight_name": "Kingfisher",
+				"depart_time": 1314938580000,
+				"arrival_time": 1314942180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 2819
+			}, {
+				"flight_no": "6E-124",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314960180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 9907
+			}, {
+				"flight_no": "SG-113",
+				"flight_name": "SpiceJet",
+				"depart_time": 1314924180000,
+				"arrival_time": 1314930780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 3953
+			}, {
+				"flight_no": "6E-118",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314945780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 1753
+			}, {
+				"flight_no": "IT-131",
+				"flight_name": "Kingfisher",
+				"depart_time": 1314938580000,
+				"arrival_time": 1314942180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 2819
+			}, {
+				"flight_no": "6E-124",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314960180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 9907
+			}, {
+				"flight_no": "SG-113",
+				"flight_name": "SpiceJet",
+				"depart_time": 1314924180000,
+				"arrival_time": 1314930780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 3953
+			}, {
+				"flight_no": "6E-118",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314945780000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 1753
+			}, {
+				"flight_no": "IT-131",
+				"flight_name": "Kingfisher",
+				"depart_time": 1314938580000,
+				"arrival_time": 1314942180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 2819
+			}, {
+				"flight_no": "6E-124",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314960180000,
+				"from_airport": "New Delhi",
+				"to_airport": "Mumbai",
+				"price": 9907
+			}],
+			"depart_on": [{
+				"flight_no": "6E-541",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314945780000,
+				"from_airport": "Mumbai",
+				"to_airport": "New Delhi",
+				"price": 1753
+			}, {
+				"flight_no": "SG-214",
+				"flight_name": "SpiceJet",
+				"depart_time": 1314924180000,
+				"arrival_time": 1314930780000,
+				"from_airport": "Mumbai",
+				"to_airport": "New Delhi",
+				"price": 1953
+			}, {
+				"flight_no": "IT-266",
+				"flight_name": "Kingfisher",
+				"depart_time": 1314938580000,
+				"arrival_time": 1314942180000,
+				"from_airport": "Mumbai",
+				"to_airport": "New Delhi",
+				"price": 2819
+			}, {
+				"flight_no": "6E-334",
+				"flight_name": "IndiGo",
+				"depart_time": 1314942180000,
+				"arrival_time": 1314960180000,
+				"from_airport": "Mumbai",
+				"to_airport": "New Delhi",
+				"price": 9917
+			}]
+				
+		};
+
+        var len = flightDetails.depart_on.length;
+        var ul_departure = document.getElementById('trip1-list');
+        var li, div, childDiv, span, icon;
+
+        for(var i = 0; i < len; i++){
+            li = document.createElement('li');
+
+            //Icon Div
+            icon = flightDetails.depart_on[i].flight_name.toLowerCase().indexOf('spice') > -1 ? 'air' : flightDetails.depart_on[i].flight_name.toLowerCase().indexOf('indigo') > -1 ? 'indigo' : 'kf';
+            div =  createElementWithAttrClass("div" , "flight-icons " + icon);
+            li.appendChild(div);
+
+            //Flight Details Div
+            div = createElementWithAttrClass( "div" ,"flight-details");
+                childDiv = createElementWithAttrClass( "div" , "time");
+                childDiv = createTextNodeAndAppendToElem(childDiv, msToTime(flightDetails.depart_on[i].depart_time) + " - " + msToTime(flightDetails.depart_on[i].arrival_time));          
+                div.appendChild(childDiv);
+                
+                childDiv = createElementWithAttrClass( "div" , "name");
+                childDiv = createTextNodeAndAppendToElem(childDiv , flightDetails.depart_on[i].flight_name + " " + flightDetails.depart_on[i].flight_no);          
+                div.appendChild(childDiv);
+
+                childDiv = createElementWithAttrClass( "div" , "duration");
+                childDiv = createTextNodeAndAppendToElem(childDiv, timeFormat( msToTime(flightDetails.depart_on[i].arrival_time - flightDetails.depart_on[i].depart_time)));
+                div.appendChild(childDiv);
+            li.appendChild(div);
+
+            //Trip Fare Div
+            div = createElementWithAttrClass( "div" ,"trip-fare");
+            div = createTextNodeAndAppendToElem(div, "Rs. " + flightDetails.depart_on[i].price);
+            li.appendChild(div);
+
+            //cb Div
+            div = createElementWithAttrClass( "div" ,"cb");       
+            span = createElementWithAttrClass( "span" ,"cbid");   
+            span = createTextNodeAndAppendToElem(span, "2"); 
+            div.appendChild(span);
+            li.appendChild(div);
+
+            //To highlight the li element selected
+            li.addEventListener("click", function(){
+                this.parentNode.childNodes.forEach(function(n){
+                    if(n.nodeName != "#text"){
+                        var attr = createAttributeWithValue("class", " ");   
+                        n.setAttributeNode(attr);                  
+                    }
+                });
+                var attr = createAttributeWithValue("class", "selected");
+                this.setAttributeNode(attr);
+            });
+
+            //Append li element to ul
+            ul_departure.appendChild(li);
+            
+        }
+
+        function createElementWithAttrClass(elemName, className){
+            var elem = document.createElement(elemName);
+            var attr = document.createAttribute("class"); 
+            attr.value = className;
+            elem.setAttributeNode(attr);
+            return elem;
+        }
+
+        function createTextNodeAndAppendToElem(elem, str){
+            var textNode = document.createTextNode(str);
+            elem.appendChild(textNode);
+            return elem;
+        }
+
+        function createAttributeWithValue(attr, val){
+            var attr = document.createAttribute(attr); 
+            attr.value = val;
+            return attr;
+        }
+
+        function msToTime(duration) {
+			var milliseconds = parseInt((duration % 1000) / 100)
+				, minutes = parseInt((duration / (1000 * 60)) % 60)
+				, hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+			hours = (hours < 10) ? "0" + hours : hours;
+			minutes = (minutes < 10) ? "0" + minutes : minutes;
+
+			return hours + ":" + minutes;
+		};
+
+        function timeFormat(duration){
+			return duration.slice(0,2) + "h:" + duration.slice(3) + "m";
+		}
+
+})()
